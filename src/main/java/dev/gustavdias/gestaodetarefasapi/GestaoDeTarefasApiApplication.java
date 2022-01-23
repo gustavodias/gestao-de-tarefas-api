cb0129caeea31107ecb9946737e379b5e0a8c150
gustavodias <gustavoemersonfd@gmail.com>
2022-01-23 16:06:51 -0300

    Extends na classe ServeletBean para iniciar os beans junto com a aplicação;

--- src/main/java/dev/gustavdias/gestaodetarefasapi/GestaoDeTarefasApiApplication.java
+++ src/main/java/dev/gustavdias/gestaodetarefasapi/GestaoDeTarefasApiApplication.java
@@ -1,10 +1,19 @@
 package dev.gustavdias.gestaodetarefasapi;
 
+import com.sun.faces.config.ConfigureListener;
+import dev.gustavdias.gestaodetarefasapi.beans.ServeletBean;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletContextInitializer;
+import org.springframework.boot.web.servlet.ServletListenerRegistrationBean;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+import javax.faces.webapp.FacesServlet;
 
 @SpringBootApplication
-public class GestaoDeTarefasApiApplication {
+public class GestaoDeTarefasApiApplication extends ServeletBean {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GestaoDeTarefasApiApplication.class, args);
