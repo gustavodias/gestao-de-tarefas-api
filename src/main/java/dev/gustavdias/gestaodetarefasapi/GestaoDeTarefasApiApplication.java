630f270d660aeb135c182786a064751d1994dac6
gustavodias <gustavoemersonfd@gmail.com>
2022-01-23 15:26:21 -0300

    First Commit

---
+++ src/main/java/dev/gustavdias/gestaodetarefasapi/GestaoDeTarefasApiApplication.java
@@ -0,0 +1,13 @@
+package dev.gustavdias.gestaodetarefasapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoDeTarefasApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoDeTarefasApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/gustavdias/gestaodetarefasapi/GestaoDeTarefasApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.gustavdias.gestaodetarefasapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoDeTarefasApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
